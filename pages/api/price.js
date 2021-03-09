5cf545d7be49ea4fa1bcd3068b3d65117a69ce52
Lee Ingram <lee@soylentcola.com>
2021-03-08 22:43:07 -0800

    Playing around

--- pages/api/price.js
+++ pages/api/price.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler (req, res) {
-  res.status(200).json({ price: '0.00296340' })
+  res.status(200).json({ nano: '0.00296340', banano: '0.000', moon:'0.000' })
 }
--- pages/pairs/NANO-BANANO.js
+++ pages/pairs/NANO-BANANO.js
@@ -1,10 +1,18 @@
 import Link from 'next/link'
 import styles from '../../styles/Pairs.module.css'
 
-
 export async function getStaticProps(context) {
-  const res = await fetch(`https://theswap.vercel.app/api/nano-banano`)
-  const data = await res.json()
+  const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=banano&vs_currencies=btc`);
+  const data = await res.json();
+  console.log(data);
+
+  const prices = {
+    "nanobanano":'0.00296340',
+    "bananonano":'0.00235000',
+  }
+
+  console.log(prices.nanobanano);
+  console.log(prices.bananonano);
 
   if (!data) {
     return {
@@ -13,11 +21,13 @@ export async function getStaticProps(context) {
   }
 
   return {
-    props: {}, 
+    props: { 
+      prices,
+     }, 
   }
 }
 
-export default function NanoBanano() {
+export default function NanoBanano(prices) {
   return (
     <>
       <main className={styles.main}>
@@ -25,16 +35,17 @@ export default function NanoBanano() {
           Swap NANO/BANANO
         </h3>
         <div className={styles.grid}>
-        <Link href="https://gonano.dev/payment/new">
-        <a className={styles.card}>
+        <Link href="#">
+        <a className={styles.bananocard}>
         <h3>Buy BANANO&rarr;</h3>
-        <p>@ {props.price}</p>
+        <p>@ 0.00296340 NANO</p>
         </a>
         </Link> 
         <Link href="#">
-        <a className={styles.card}>
+        <a className={styles.bananocard}>
         <h3>Sell BANANO &rarr;</h3>
-        <p>@ (NANO/BANANO rate)</p>
+        <p>@ 0.00235000 NANO</p>
+        { prices.bananonano }
         </a>
         </Link> 
         </div>
