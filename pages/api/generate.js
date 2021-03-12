b602191ce659fa8d7897200a2176c69ee23a92f7
Lee Ingram <lee@soylentcola.com>
2021-03-12 14:14:31 -0800

    Adding call to wallet

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler (req, res) {
-    
-    res.status(200).json({ req })
+    fetch('http://ec2-34-226-15-179.compute-1.amazonaws.com:11338',{
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify({
+            "action":"account_create",
+            "wallet": process.env.BANANO_WALLET_SEED
+        })
+    })
+    .then( data => res.status(200).json(data) )
+    .catch( error => console.log(error) )
+    //res.status(200).json({  })
   }
   
