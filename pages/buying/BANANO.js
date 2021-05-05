eadc83d803b86953d846e936575704f33f934283
Lee Ingram <lee@soylentcola.com>
2021-05-04 19:08:28 -0700

    Adding warnings

--- pages/buying/BANANO.js
+++ pages/buying/BANANO.js
@@ -146,6 +146,7 @@ export default function BuyingBanano({initialData}) {
         </div>
         <center>
         <div className="btn-group" role="group" aria-label="Confirm or Cancel">
+          <small>Do NOT send more than the amount displayed during the payment prompt. Any overages will be kept.</small>
         <button className="btn btn-primary" type="submit">Confirm</button>
         <button className="btn btn-primary" type="reset">Reset</button>
         </div>
